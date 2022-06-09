e58e339bb61e8db00dc23266f6845b6143f0d0dc
Carlos Borges <carlosmateus7720@gmail.com>
2022-06-09 19:33:09 -0300

    muitas coisas feitas

--- src/main.go
+++ src/main.go
@@ -1,32 +1,47 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-	var nome = "Carlos"
+	exibeintroducao()
+	exibeMenu()
 
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Nao conheco esse comando")
+		os.Exit(-1)
+
+	}
+}
+
+func exibeintroducao() {
+	var nome = "Carlos"
+	var versao = 1.18
 	fmt.Println("Seja Bem-Vindo sr.", nome)
+	fmt.Println("Esse programa esta na ", versao)
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scanf("%d", &comandoLido)
+	fmt.Println("O comando escolhido foi:", comandoLido)
+	return comandoLido
+}
+
+func exibeMenu() {
 
 	fmt.Println("1- Iniciar monitoriamento")
 	fmt.Println("2- exibir os logs")
 	fmt.Println("0- sair do programa")
-
-	var comando int
-
-	fmt.Scanf("%d", &comando)
-	fmt.Println("O comando escolhido foi:", comando)
-
-	if comando == 1 {
-		fmt.Println("Monitorando...")
-
-	} else if comando == 2 {
-		fmt.Println("Exibindo Logs...")
-
-	} else if comando == 0 {
-		fmt.Println("Saindo do programa...")
-
-	} else {
-		fmt.Println("Nao conheco esse comando")
-	}
 
 }
